a38049bbb524cdb03f3f0881ba7964e454f5670f
Alex Pleava <alex.pleava@gmail.com>
2026-04-08 12:22:56 -0400

    Teacher training now works as an env withn TD-MPC2 scripts

--- tdmpc2/envs/__init__.py
+++ tdmpc2/envs/__init__.py
@@ -102,7 +102,7 @@ def make_env(cfg):
             cfg.obs_shape = {k: v.shape for k, v in env.observation_space.spaces.items()}
         except: # Box
             cfg.obs_shape = {cfg.get('obs', 'state'): env.observation_space.shape}
-        cfg.action_dim = env.action_space.shape[0]
+        cfg.action_dim = env.action_space.shape[-1]
         cfg.state_dim = env.state_dim
         cfg.episode_length = env.max_episode_steps
         cfg.seed_steps = max(1000, 5*cfg.episode_length)
